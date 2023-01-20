e07256bd469f0cc8c93b76b9ea82275a18fc9689
Zihan Chen <me@zihan.ga>
2023-01-20 17:10:23 +0800

    🎉

---
+++ src/components/Test.tsx
@@ -0,0 +1,10 @@
+import type { Component } from "solid-js"
+import { Hoverable } from "./Curzor"
+
+export const Test: Component = () => {
+	return (
+		<Hoverable>
+			<a href="//helloworld.com">Hello world</a>
+		</Hoverable>
+	)
+}
